a2f6f4fc14a4631168e5df07e13a88d226cd44ee
Chalesgo <charleskristoffer.go@gmail.com>
2024-01-15 05:53:56 +0800

    Update index.js

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -7,7 +7,6 @@ const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 const x = randomNumber;
-console.log(x)
 if (x > 50){
     console.log("more than fifty");
 }
@@ -35,6 +34,9 @@ else if (x%5 == 0){
 else if (x%3 == 0){
     console.log("fizz");
 }
+else {
+    console.log(x)
+}
 
 let toDisplay = ""
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
@@ -60,38 +62,46 @@ const list = ["apple", "banana", "cherry", "date", "elderberry"];
 let z = 0;
 let text = "";
 while(list[z]){
-    text += list[z];
+    text += list[z] + "\n";
     z++;
     
 }
 console.log(text); 
 
 // Checkpoint 2.3 What is the difference between do while and while loop?
-// Answer: 
+// Answer: a do while will execute some code before the condition
 
 // TODO 2.7 Use a for of loop to display all the values in the list
-// Your code here
+for (const value of list) {
+    console.log(value);
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
-// Your code here
+for (const index in list) {
+    console.log(list[index]);
+}
 
 // TODO 2.9 Use the for each method of the list to display all its values 
-// Your code here
+list.forEach(value => {
+    console.log(value);
+});
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+// Answer: Use forof when iterating over values in arrays, forin for iterating over object properties, and forEach for array iterations with the need to perform an operation on each element.
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
 const numerator = Math.floor((Math.random() * 100) + 1);
 const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
 
-
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
+try{
+    if(denominator === 0){
+        throw new Error("Division by zero error")
+    } else {
+        console.log(numerator / denominator);
+    }
+} catch (error) {
+    console.error("Error:", error.message);
+} finally {
+    console.log("Cleaning up resources");
 }
-
-
-
