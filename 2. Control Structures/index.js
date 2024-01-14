6bcca7bd398a28cf2de33bf1d1801eeec32a8a3c
Chalesgo <charleskristoffer.go@gmail.com>
2024-01-15 05:40:32 +0800

    Update index.js

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -6,31 +6,65 @@
 const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
-// Your code here
+const x = randomNumber;
+console.log(x)
+if (x > 50){
+    console.log("more than fifty");
+}
+else{
+    console.log("less than fifty");
+}
 
 // TODO 2.2 Display whether the random number is odd or even
-// Your code here
+if(x%2 === 0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
-// Your code here
+if (x%3 == 0 && x%5 == 0){
+    console.log("fizzbuzz");
+}
+else if (x%5 == 0){
+    console.log("buzz");
+}
+else if (x%3 == 0){
+    console.log("fizz");
+}
 
 let toDisplay = ""
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
-// Your code here
+if(x%2 == 0){
+    toDisplay += "Even";
+}
+else{
+    toDisplay += "Odd";
+}
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: using switch statement when there are multiple choices to use by the user
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
 const n = 10
-// Your code here
+for (let i = 1;i<=n; i++ ){
+    console.log(i)
+}
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
-// Your code here
+let z = 0;
+let text = "";
+while(list[z]){
+    text += list[z];
+    z++;
+    
+}
+console.log(text); 
 
 // Checkpoint 2.3 What is the difference between do while and while loop?
 // Answer: 
