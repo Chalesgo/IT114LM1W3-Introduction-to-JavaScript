c6d96378d048f1bf4c2d405e1a6936a7251c19e1
Chalesgo <charleskristoffer.go@gmail.com>
2024-01-15 05:36:41 +0800

    Update index.js

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -4,43 +4,51 @@
 */
 
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
-// Your code here
-
 // TODO 1.2 Use the let keyword to define a variable.
-// Your code here
-
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
-// Your code here
+var x = 10;
+let y = 69;
+const z = 24;
+console.log(z)
 
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: const cannot be changed be it value or type and var is for old windows
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
-// Your code here
+var a = 5
+const b = 10/2;
+let c = a + b;
+console.log(c)
 
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
-// Your code here
+// Answer: addition and multiplication
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
-// Your code here
+let mystring = "Hello";
+mystring += "there";
+console.log(mystring)
 
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: a concatinated addition of strings 
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
-// Your code here
+const x1 = 5
+const y1 = 10
+const n = x1 && y1;
+console.log(n)
 
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: logical and
 
 
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
-
+const jedi = ["luke","obi-wan","anakin"];
+2 in jedi;
+console.log(jedi)
 
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
 // Answer: 
